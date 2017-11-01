d4418aa72d7c372e2017fd271ca7859a3a5ca6a6
JochoVoled <johannes@smidelov.com>
2017-11-01 14:19:09 +0100

    Added fake Game and Match for Testing

---
+++ BowlingTestBase/FakeMatch.cs
@@ -0,0 +1,48 @@
+﻿using BowlingLib.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BowlingLib.Model;
+
+namespace BowlingTestBase
+{
+    class FakeMatch : IMatch
+    {
+        private KeyValuePair<Member, IGame> playerOne { get; set; }
+        private KeyValuePair<Member, IGame> playerTwo { get; set; }
+        public KeyValuePair<Member, IGame> PlayerOne { get => playerOne; }
+        public KeyValuePair<Member, IGame> PlayerTwo { get => playerTwo; }
+
+
+        public int WinnerId
+        {
+            get
+            {
+                if (WinnerId == 0) return CalculateWinner();
+                return WinnerId;
+            }
+            set { WinnerId = value; }
+        }
+
+        public FakeMatch(Member PlayerOne, Member PlayerTwo)
+        {
+            playerOne = new KeyValuePair<Member, IGame>(PlayerOne, new FakeGame());
+            playerTwo = new KeyValuePair<Member, IGame>(PlayerTwo, new FakeGame());
+
+            WinnerId = 0;
+        }
+
+        public int CalculateWinner()
+        {
+            if (playerOne.Value.Score > playerTwo.Value.Score)
+                return playerOne.Key.MemberId;
+            return playerTwo.Key.MemberId;
+            //throw new NotImplementedException();
+        }
+
+        public void Play()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
