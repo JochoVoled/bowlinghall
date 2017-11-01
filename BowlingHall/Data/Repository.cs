ba7508ddb16cfa54cd5b7ff1e80d71cc73481fa3
JochoVoled <johannes@smidelov.com>
2017-11-01 14:19:01 +0100

    Implemented interface changes on real Repository

--- BowlingHall/Data/Repository.cs
+++ BowlingHall/Data/Repository.cs
@@ -8,6 +8,19 @@ namespace BowlingLib.Data
 {
     public class Repository : IRepository
     {
+        private static BowlingContext _context { get; set; }
+
+        public BowlingContext Context
+        {
+            get {
+                if (_context == null)
+                {
+                    _context = new BowlingContext();
+                }
+                return _context;
+            }
+        }
+
         public DatabaseResultState Create(Competition competition)
         {
             throw new NotImplementedException();
@@ -18,7 +31,17 @@ namespace BowlingLib.Data
             throw new NotImplementedException();
         }
 
+        public IEnumerable<Competition> GetAllCompetition()
+        {
+            throw new NotImplementedException();
+        }
+
         public Competition GetById<Competition>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Competition GetCompetitionById(int id)
         {
             throw new NotImplementedException();
         }
