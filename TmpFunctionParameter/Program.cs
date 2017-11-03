bdba28443a7cfce22a93d9c72c606a8f32c12b4d
JochoVoled <johannes@smidelov.com>
2017-11-03 15:40:18 +0100

    Created a temporary project for learning delegates

---
+++ TmpFunctionParameter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TmpFunctionParameter
+{
+    class Program
+    {
+        delegate void MyHandler();
+        static void Main(string[] args)
+        {
+            Add(Hello);
+        }
+        private static void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+        private static void Add(MyHandler myHandler)
+        {
+            myHandler();
+            myHandler();
+        }
+    }
+}
