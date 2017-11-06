9a79030a90a57c5c08610cbdc21e76f5ad4bff68
Johannes Smidelöv <johannes@smidelov.com>
2017-11-06 19:42:47 +0100

    Implemented a unit test for Creating, Reading and Updating Competition Info

--- BowlingIntegrationTests/ValidateRequirements.cs
+++ BowlingIntegrationTests/ValidateRequirements.cs
@@ -1,20 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BowlingLib.Data;
+using BowlingLib.Model;
+using BowlingLib.Model.Interfaces;
 using Xunit;
 
 namespace BowlingIntegrationTests
 {
-    class ValidateRequirements
+    public class ValidateRequirements
     {
-        //Verifiera med integrationstester attdet går att generera, lagra och hämta tävlingsinformation.
         [Fact]
         public void CruCompetitionInfo()
         {
+            Repository repo = new Repository();
+            Competition competition = new Competition {
+                CompetitionId = 999
+            };
             // Create competition, assert
-            // Read competition, assert
-            // Update competition, assert
+            var cResult = repo.Create(competition);
+            Assert.Equal(DatabaseResultState.successful, cResult);
 
+            // Read competition, assert
+            var rResult = repo.GetCompetitionById(competition.CompetitionId);
+            Assert.Equal(competition.CompetitionId, rResult.CompetitionId);
+            
+            // Update competition, assert
+            competition.Matches.Add(new Match(new Member(4), new Member(5)));
+            var uResult = repo.Update(competition);
+            Assert.Equal(DatabaseResultState.successful, uResult);
         }
 
         //Verifiera med integrationstester attdet går att generera, lagra och hämta medlemmar.
