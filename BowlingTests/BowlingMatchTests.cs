d13c735dfba76d1f24bbebb6b54cecce39350b2b
Johannes Smidelöv <johannes@smidelov.com>
2017-11-07 19:53:32 +0100

    Changed the CanDecide... and BestWinRatio... unit tests to use FakeTest. Added new CanPlay to test Play, to catch bugs and exceptions

--- BowlingTests/BowlingMatchTests.cs
+++ BowlingTests/BowlingMatchTests.cs
@@ -20,17 +20,15 @@ namespace BowlingTests
             repo.Create(new Member(2));
             Member member2 = repo.GetMemberById(2);
 
-            // TODO implement Lane crud methods to FakeRepository
             repo.Create(new Lane(1));
             Lane lane = repo.GetLaneById(1);
+                        
+            repo.Create(new Match(member1,member2));
+            Match sut = repo.GetMatchByPlayers(member1, member2)[0];
 
-            // TODO implement Match crud to FakeRepository
-            //FakeMatch sut = repo.GetMatchByPlayers(member1, member2);
-            FakeMatch sut = InMemDb.Db.Matches.Find(x => x.PlayerOne.Key.MemberId == 1 && x.PlayerTwo.Key.MemberId == 2);
-
-            sut.Play();
+            sut.FakePlay();
             int winnerId = sut.CalculateWinner();
-            
+
             Assert.Equal(2, winnerId);
         }
         [Fact]
@@ -41,15 +39,31 @@ namespace BowlingTests
             Member member1 = repo.GetMemberById(1);
             repo.Create(new Member(2));
             Member member2 = repo.GetMemberById(2);
-            repo.Create(new Competition(1, new List<Member> { member1, member2}));
+            repo.Create(new Competition(1, new List<Member> { member1, member2 }));
 
             Competition sut = (Competition)repo.GetCompetitionById(1);
             sut.Matches.ForEach(x => x.Play());
-            
+
             // Call GetChampion from Competition class
             Member winner = sut.GetChampion();
-            
+
             Assert.Equal(member2.MemberId, winner.MemberId);
+        }
+
+        [Fact]
+        public void CanPlay() {
+            IRepository repo = new FakeRepository();
+            repo.Create(new Member(1));
+            Member member1 = repo.GetMemberById(1);
+            repo.Create(new Member(2));
+            Member member2 = repo.GetMemberById(2);
+
+            repo.Create(new Lane(1));
+            Lane lane = repo.GetLaneById(1);
+            repo.Create(new Match(member1, member2));
+            Match sut = repo.GetMatchByPlayers(member1, member2)[0];
+
+            sut.Play();
         }
     }
 }
